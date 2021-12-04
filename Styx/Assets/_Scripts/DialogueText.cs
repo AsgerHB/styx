732a821e4cb46823f3e72c8ae1fdf814b5b6ca51
Asger HB <asgerhb@gmail.com>
2021-12-04 21:10:45 +0100

    Add the actual dialogue

--- Styx/Assets/_Scripts/DialogueText.cs
+++ Styx/Assets/_Scripts/DialogueText.cs
@@ -5,7 +5,37 @@ using UnityEngine;
 public class DialogueText
 { 
     public static string[] INTRODUCTION = {
-        @"i've come to make an announcement:"
+@"<color=grey>Passenger:</color>
+
+<color=blue>I didn't do it...</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>What?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I didn't kill her.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>I believe you...</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>The jury didn't. They sentenced me to life in prison. I was 18!</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>That wasn't fast enough, so they sent me to the chair.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>You were not executed for her death. </color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>No, that was... That was something else.</color>",
     };
 
 
@@ -13,29 +43,110 @@ public class DialogueText
     {
          new string [] 
          { 
-            "shadow the hedgehog's a bitch-ass motherfucker." ,
-            "he pissed on my fucking wife." ,
-            "that's right." ,
-            "he took his hedgehog fuckin' quilly dick out and he pissed on my fucking wife, and he said his dick was this big, and i said that's disgusting." ,
-            "so i'm making a callout post on my twitter.com." ,
-            "shadow the hedgehog, you got a small dick." ,
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Ah, you understand. Good.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>When we get to heaven, I'll take care of you.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>As you took care of your partner?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I...</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>He was a necessary sacrifice.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>When you fight a war, brothers become soldiers.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>All in the service of power.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Spoken like someone who's always had it.</color>",
          },
          new string [] 
          { 
-            "it's the size of this walnut except way smaller." ,
-            "and guess what? here's what my dong looks like." ,
-            "that's right, baby." ,
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Look, I'm not saying I'm perfect but...</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>What do you do when a random stranger tries to hold you down?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>You make him stop.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>You make sure he can't try again.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>He wasn't supposed to die though.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>But he did.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Well, I wasn't supposed to go to prison, but here we are.</color>",
+
          },
          new string [] 
          {
-            "tall points, no quills, no pillows, look at that, it looks like two balls and a bong." ,
-            "he fucked my wife, so guess what, i'm gonna fuck the earth." ,
+@"<color=grey>Passenger:</color>
+
+<color=blue>Why did that stupid bastard have to try and stop me?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>When he grabbed me, I just saw them.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>My father, the judge, Isac... Those spineless prison bastards.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I lost control. He didn't even make it to the hospital.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Only took a couple of weeks to get the death sentence. </color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I knew the exact date of my death and I could do nothing to stop it.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>...</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I've suffered enough, haven't I?</color>",
          },
          new string[] 
          {
-            "that's right, this is what you get! my super laser piss! except i'm not gonna piss on the earth." ,
-            "i'm gonna go higher." ,
-            "i'm pissing on the moooon! how do you like that, obama? i pissed on the moon, you idiot! you have twenty-three hours before the piss droplets hit the fucking earth, now get out of my fucking sight before i piss on you too!"
+@"<color=grey>Passenger:</color>
+
+<color=blue>I'm finally home.</color>",
          }
     
     
@@ -46,29 +157,138 @@ public class DialogueText
     {
         new string [] 
         { 
-            "SHADOW THE HEDGEHOG'S A BITCH-ASS MOTHERFUCKER.",
-            "HE PISSED ON MY FUCKING WIFE.",
-            "THAT'S RIGHT.",
-            "HE TOOK HIS HEDGEHOG FUCKIN' QUILLY DICK OUT AND HE PISSED ON MY FUCKING WIFE, AND HE SAID HIS DICK WAS THIS BIG, AND I SAID THAT'S DISGUSTING.",
-            "SO I'M MAKING A CALLOUT POST ON MY TWITTER.COM.",
-            "SHADOW THE HEDGEHOG, YOU GOT A SMALL DICK.",
+@"<color=grey>Passenger:</color>
+
+<color=blue>Look, you wouldn't be the first to think I belong in hell.</color>",
+
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>But you don't know the whole story.</color>",
+
+
+@"<color=grey>Charon:</color>
+
+<color=red>I know all...</color>",
+
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Then you know that prison is a dog eat dog world.</color>",
+
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>The dogs were circling and I wasn't to wait around for their bite.</color>",
+
+
+@"<color=grey>Charon:</color>
+
+<color=red>...</color>",
+
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I wasn't even supposed to be in jail in the first place!</color>",
         },
         new string [] 
         { 
-            "IT'S THE SIZE OF THIS WALNUT EXCEPT WAY SMALLER.",
-            "AND GUESS WHAT? HERE'S WHAT MY DONG LOOKS LIKE.",
-            "THAT'S RIGHT, BABY.",
+@"<color=grey>Passenger:</color>
+
+<color=blue>Understand, I have been under somebody's boot my entire life.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>My father, then the prison system... and then finally Isac.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Until one day I snapped.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Isac may have been a monster, but I had sharper teeth.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Once the dust settled, hell became a paradise... For me.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I felt powerful, in control.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>What about shame?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I was done suffering.</color>",
         },
         new string [] 
         {
-            "TALL POINTS, NO QUILLS, NO PILLOWS, LOOK AT THAT, IT LOOKS LIKE TWO BALLS AND A BONG.",
-            "HE FUCKED MY WIFE, SO GUESS WHAT, I'M GONNA FUCK THE EARTH.",
+
+@"<color=grey>Charon</color>
+
+<color=red>We're arriving at damnation.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Honestly, what did you expect of me?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>I admit, I haven't lived the cleanest life. But, how could I?</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>My entire life has been covered in filth.</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>You could have washed it clean.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>The prison? I was just a man!</color>",
+
+@"<color=grey>Charon:</color>
+
+<color=red>You were a king.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>...</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Hahahaha!</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>So was Isac, so was the bastard that replaced me.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>But you're right.</color>",
+
+@"<color=grey>PASSENGER</color>
+
+I should have saved them from the hell they created for themselves. ",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>It is a burden, us, the powerful must bear: compassion.</color>",
+
+@"<color=grey>Passenger:</color>
+
+<color=blue>Don't you agree, ferry man?</color>",
         },
         new string[] 
         {
-            "THAT'S RIGHT, THIS IS WHAT YOU GET! MY SUPER LASER PISS! EXCEPT I'M NOT GONNA PISS ON THE EARTH.",
-            "I'M GONNA GO HIGHER.",
-            "I'M PISSING ON THE MOOOON! HOW DO YOU LIKE THAT, OBAMA? I PISSED ON THE MOON, YOU IDIOT! YOU HAVE TWENTY-THREE HOURS BEFORE THE PISS DROPLETS HIT THE FUCKING EARTH, NOW GET OUT OF MY FUCKING SIGHT BEFORE I PISS ON YOU TOO! "
+@"<color=grey>Passenger:</color>
+
+<color=blue>It will never end, will it?</color>",
         }
     };
 }
--- Styx/Assets/_Scripts/StateMachine/CharonState.cs
+++ Styx/Assets/_Scripts/StateMachine/CharonState.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharonState : State
 {
+    string[] _texts;
+    int _progress = 0;
+
     public CharonState(StateSystem system) : base(system)
     {
     }
@@ -12,20 +16,32 @@ public class CharonState : State
     {
         Debug.Log("Entering: Charon State");
         _system.AnyKey.Show();
+        _system.DialogueBox.SetActive(true);
+
+        _texts = DialogueText.INTRODUCTION;
+        _system.DialogueBox.GetComponent<Text>().text = _texts[_progress];
+
         yield return new WaitForSeconds(0f);
     }
 
     public override IEnumerator Accept()
     {
         Debug.Log("Accept Button Pressed...");
-        Debug.Log("...Changing States!");
+        if (_progress +1 < _texts.Length)
+        {
+            _progress += 1;
+            _system.DialogueBox.GetComponent<Text>().text = _texts[_progress];
+        }
+        else
+        {
+            Debug.Log("...Changing States!");
 
+            _system.cube.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(0f, 1.4f, 1.4f));
 
-        _system.cube.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(0f, 1.4f, 1.4f));
+            _system.AnyKey.Hide();
 
-        _system.AnyKey.Hide();
-
-        _system.SetState(new PassangerState(_system));
-        yield return new WaitForSeconds(0f);
+            _system.SetState(new PassangerState(_system));
+            yield return new WaitForSeconds(0f);
+        }
     }
 }
